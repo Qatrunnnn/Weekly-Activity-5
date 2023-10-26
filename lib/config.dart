db3da464c2eef94d830f004b71da75ffd2555b02
Raihan Qatrunada <113822753+Qatrunnnn@users.noreply.github.com>
2023-10-26 14:54:32 +0700

    Tugas 5
    tugas minggu ke 5 update etter

---
+++ lib/config.dart
@@ -0,0 +1,4 @@
+String token = '65307bb29b493f4b9fe2578a';
+String project = 'office';
+String appid = '65308c309b493f4b9fe25907';
+String fileuri = 'https://file.etter.cloud./d226fd9f5fcf8bc3cbdff22e2bd79efe/';
